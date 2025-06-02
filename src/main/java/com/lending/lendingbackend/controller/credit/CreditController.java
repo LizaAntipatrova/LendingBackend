89f9cbb3ec1abca7a7816768ccef38b5ef5b0e90
Liza Antipatrova <Liza.Antipa@gmail.com>
2025-06-02 15:45:16 +0300

    confirm credit

--- src/main/java/com/lending/lendingbackend/controller/credit/CreditController.java
+++ src/main/java/com/lending/lendingbackend/controller/credit/CreditController.java
@@ -9,7 +9,7 @@ import com.lending.lendingbackend.service.CreditService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,13 +18,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class CreditController {
     private final CreditService creditService;
     @GetMapping("/client/credit")
-    public String showApplicationPage(Model model, @RequestParam("id") Long id) {
+    public String showClientCredit(Model model, @RequestParam("id") Long id) {
         CreditDTO creditDTO = creditService.getCreditByContractNumber(id);
         model.addAttribute("creditStatusList", CreditStatus.getStatusTitles());
         model.addAttribute("paymentTypeList", PaymentType.getTypeTitles());
         model.addAttribute("creditDTO", creditDTO);
         return "credit_look";
     }
+    @GetMapping("/manager/credit")
+    public String showManagerCredit(Model model, @RequestParam("id") Long id) {
+        CreditDTO creditDTO = creditService.getCreditByContractNumber(id);
+        model.addAttribute("creditStatusList", CreditStatus.getStatusTitles());
+        model.addAttribute("paymentTypeList", PaymentType.getTypeTitles());
+        model.addAttribute("creditDTO", creditDTO);
+        if(creditDTO.getStatus().equals(CreditStatus.AWAITING_CONFIRMATION)){
+            return "manager_credit_look";
+        }
+        return "credit_look";
+    }
 
+    @PostMapping("/manager/credit/confirm")
+    public String confirmCredit(@RequestParam("id") Long id) {
+        creditService.updateCreditStatusToActive(id);
+        return "redirect:/manager/credit?id=" + id;
+    }
 
 }
